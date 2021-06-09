b38fb7460439ff4d7eef56853a13d411583e16f1
eXCoreX <excoreky@gmail.com>
2021-06-09 19:58:52 +0300

    Initial Commit

---
+++ eKreativeTestAssignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eKreativeTestAssignment
+//
+//  Created by Rostyslav Litvinov on 09.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eKreativeTestAssignment/eKreativeTestAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  eKreativeTestAssignmentApp.swift
+//  eKreativeTestAssignment
+//
+//  Created by Rostyslav Litvinov on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct eKreativeTestAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
